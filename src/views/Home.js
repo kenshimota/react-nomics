a34d43fecbe86beed54e852b67ff9909a2775496
Jessica Moreno <jesslomasbello@gmail.com>
2022-05-12 23:04:14 -0400

    cree un enlace

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home(){
     return (
         <div>
+            <Link to="/product">
+                ir a product
+            </Link>
+            <br/>
             Hola Mundo
         </div>
     );
