48e46676d58da05d0e48bac65696a41e920a2cf5
Jessica Moreno <jesslomasbello@gmail.com>
2022-05-12 23:03:02 -0400

    agregue rutas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
+import Product from "./views/productos";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path = '/' element = { <Home />} />
+          <Route path = "/product" element = { <Product/> } />
         </Routes>
       </BrowserRouter>
     </div>
