0ab55c41e739f6da3268d6c872966671772a40e3
Jessica Moreno <jesslomasbello@gmail.com>
2022-05-12 22:37:04 -0400

    agregando rutas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
 
 function App() {
   return (
     <div className = 'App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path = '/' element = { <Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
