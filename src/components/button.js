a1f32fffe26e32ff9ff7850b1e75ef096100f9bf
Jessica Moreno <jesslomasbello@gmail.com>
2022-05-14 18:58:46 -0400

    cree un button

---
+++ src/components/button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+function Btn(){
+    return <div>
+         <Button variant="contained" color="primary">
+              Success
+          </Button>
+    </div>
+}
+
+export default Btn;
