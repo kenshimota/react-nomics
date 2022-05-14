0743b77adfd4cbf7447c06455e52bc7cb235a8f8
Jessica Moreno <jesslomasbello@gmail.com>
2022-05-14 18:58:16 -0400

    agregue un component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Product from "./views/productos";
+import SearchAppBar from "./components/SearchAppBar";
 
 function App() {
   return (
     <div className = 'App'>
       <BrowserRouter>
+      <SearchAppBar/>
         <Routes>
           <Route path = '/' element = { <Home />} />
           <Route path = "/product" element = { <Product/> } />
